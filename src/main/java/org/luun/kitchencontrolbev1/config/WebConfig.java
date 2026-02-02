5202678d8c98ac34c00afced31453d77b509b86f
luunguyen3112005@gmail.com <luunguyen3112005@gmail.com>
2026-02-02 18:03:06 +0700

    Adding: config file

---
+++ src/main/java/org/luun/kitchencontrolbev1/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.luun.kitchencontrolbev1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://swp-web-six.vercel.app", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
