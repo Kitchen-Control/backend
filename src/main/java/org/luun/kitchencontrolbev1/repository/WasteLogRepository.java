a90cd8ae3ac5eefaee33a8b17cb320ddba3e584f
DucAnh <nguyenduyducanh19102005@gmail.com>
2026-03-28 20:49:59 +0700

    Update for report function

--- src/main/java/org/luun/kitchencontrolbev1/repository/WasteLogRepository.java
+++ src/main/java/org/luun/kitchencontrolbev1/repository/WasteLogRepository.java
@@ -3,5 +3,9 @@ package org.luun.kitchencontrolbev1.repository;
 import org.luun.kitchencontrolbev1.entity.WasteLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface WasteLogRepository extends JpaRepository<WasteLog, Integer> {
+    List<WasteLog> findByCreatedAtBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
--- src/main/java/org/luun/kitchencontrolbev1/service/WasteLogService.java
+++ src/main/java/org/luun/kitchencontrolbev1/service/WasteLogService.java
@@ -3,9 +3,11 @@ package org.luun.kitchencontrolbev1.service;
 import org.luun.kitchencontrolbev1.dto.request.WasteLogRequest;
 import org.luun.kitchencontrolbev1.dto.response.WasteLogResponse;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface WasteLogService {
     List<WasteLogResponse> getAllWasteLogs();
     WasteLogResponse createWasteLog(WasteLogRequest request);
+    List<WasteLogResponse> getWasteLogsByDateRange(LocalDate startDate, LocalDate endDate);
 }
